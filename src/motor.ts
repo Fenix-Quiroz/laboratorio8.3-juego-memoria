5bf5922c81d649eb6fab5f6e504dad6b1bdee167
Fenix <fenixquiroz10@gamil.com>
2025-03-06 06:54:05 -0500

    codigo mejorado

--- src/motor.ts
+++ src/motor.ts
@@ -1,4 +1,4 @@
-import {  Carta ,Tablero } from './modelo'
+import { Carta, Tablero } from "./modelo";
 /*
 En el motor nos va a hacer falta un método para barajar cartas
 */
@@ -6,68 +6,89 @@ const barajarCartas = (cartas: Carta[]): Carta[] => {
   //...
   for (let i = cartas.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [cartas[i], cartas[j]] = [cartas[j], cartas[i]];
+    [{ ...cartas[i] }, {...cartas[j]}] = [cartas[j], cartas[i]];
   }
-  return cartas
-}
+  return cartas;
+};
 
 /*
   Una carta se puede voltear si no está encontrada y no está ya volteada, o no hay dos cartas ya volteadas
 */
-export const sePuedeVoltearLaCarta = (tablero: Tablero, indice: number): boolean => {
+export const sePuedeVoltearLaCarta = (
+  tablero: Tablero,
+  indice: number
+): boolean => {
   //..
-  const carta = tablero.cartas[indice]
-  return carta.encontrada === false && carta.estaVuelta === false && tablero.estadoPartida !== 'DosCartasLevantadas'
-}
-
-export const voltearLaCarta = (tablero: Tablero, indice: number) : void => {
-  //...
-  tablero.cartas = tablero.cartas.map((carta, i) =>
-    i === indice ? { ...carta, estaVuelta: true } : carta
+  const carta = tablero.cartas[indice];
+  return (
+    carta.encontrada === false &&
+    carta.estaVuelta === false &&
+    tablero.estadoPartida !== "DosCartasLevantadas"
   );
+};
 
-}
+export const voltearLaCarta = (tablero: Tablero, indice: number): void => {
+  //...
+  // tablero.cartas = tablero.cartas.map((carta, i) =>
+  //   i === indice ? { ...carta, estaVuelta: true } : carta
+  // );
+  tablero.cartas[indice].estaVuelta = true;
+  if (tablero.estadoPartida === "CeroCartasLevantadas") {
+    tablero.indiceCartaVolteadaA = indice;
+    tablero.estadoPartida = "UnaCartaLevantada";
+  } else if (tablero.estadoPartida === "UnaCartaLevantada") {
+    tablero.estadoPartida = "DosCartasLevantadas";
+    tablero.indiceCartaVolteadaB = indice;
+  }
+};
 
 /*
   Dos cartas son pareja si en el array de tablero de cada una tienen el mismo id
 */
-export const sonPareja = (indiceA: number, indiceB: number, tablero: Tablero): boolean => {
+export const sonPareja = (
+  indiceA: number,
+  indiceB: number,
+  tablero: Tablero
+): boolean => {
   //...
-  console.log(
-    `Comparando idFoto: ${tablero.cartas[indiceA].idFoto} vs ${tablero.cartas[indiceB].idFoto}`
-  );
-  return  tablero.cartas[indiceA].idFoto === tablero.cartas[indiceB].idFoto
-
-}
+  return tablero.cartas[indiceA].idFoto === tablero.cartas[indiceB].idFoto;
+};
 
 /*
   Aquí asumimos ya que son pareja, lo que hacemos es marcarlas como encontradas y comprobar si la partida esta completa.
 */
-export const parejaEncontrada = (tablero: Tablero, indiceA: number, indiceB: number) : void => {
+export const parejaEncontrada = (
+  tablero: Tablero,
+  indiceA: number,
+  indiceB: number
+): void => {
   //...
- tablero.cartas[indiceA].encontrada = true
- tablero.cartas[indiceB].encontrada = true
- console.log(esPartidaCompleta(tablero))
-}
+  tablero.cartas[indiceA].encontrada = true;
+  tablero.cartas[indiceB].encontrada = true;
+};
 
 /*
   Aquí asumimos que no son pareja y las volvemos a poner boca abajo
 */
-export const parejaNoEncontrada = (tablero: Tablero, indiceA :number, indiceB : number) : void => {
+export const parejaNoEncontrada = (
+  tablero: Tablero,
+  indiceA: number,
+  indiceB: number
+): void => {
   // ...
-  const cartaA = tablero.cartas[indiceA]
-  const cartaB = tablero.cartas[indiceB]
-  cartaA.estaVuelta = false
-  cartaB.estaVuelta = false
-}
+  const cartaA = tablero.cartas[indiceA];
+  const cartaB = tablero.cartas[indiceB];
+  cartaA.estaVuelta = false;
+  cartaB.estaVuelta = false;
+};
 
 /*
   Esto lo podemos comprobar o bien utilizando every, o bien utilizando un contador (cartasEncontradas)
 */
-export const esPartidaCompleta = (tablero: Tablero) : boolean => {
+export const esPartidaCompleta = (tablero: Tablero): boolean => {
   //...
-  return tablero.cartas.every(carta => carta.encontrada)
-}
+  return tablero.cartas.every((carta) => carta.encontrada);
+};
 
 /*
 Iniciar partida
@@ -75,6 +96,6 @@ Iniciar partida
 
 export const iniciaPartida = (tablero: Tablero): void => {
   //...
-  tablero.estadoPartida = 'CeroCartasLevantadas'
-  barajarCartas(tablero.cartas)
+  tablero.estadoPartida = "CeroCartasLevantadas";
+  barajarCartas(tablero.cartas);
 };
--- src/ui.ts
+++ src/ui.ts
@@ -1,4 +1,4 @@
-import { partida, tablero } from "./modelo";
+import { partida, Tablero, tablero } from "./modelo";
 import {
   esPartidaCompleta,
   iniciaPartida,
@@ -47,7 +47,7 @@ const animacionesAlVoltearCarta = (carta: Element): void => {
   carta.classList.add("cambiar-color-fondo");
 };
 
-const eliminarClasesDeAnimaciones = () :void => {
+const eliminarClasesDeAnimaciones = (): void => {
   const cartaA = document.querySelector(
     `.carta:nth-child(${tablero.indiceCartaVolteadaA! + 1}) `
   );
@@ -80,40 +80,44 @@ const ocultarImagenesDeParejaIncorrecta = (): void => {
   }
 };
 
-const verificacionDeParejas = (carta: Element) : void => {
-  if (
-    sonPareja(
-      tablero.indiceCartaVolteadaA!,
-      tablero.indiceCartaVolteadaB!,
-      tablero
-    )
-  ) {
-    parejaEncontrada(
-      tablero,
-      tablero.indiceCartaVolteadaA!,
-      tablero.indiceCartaVolteadaB!
-    );
-    if (esPartidaCompleta(tablero)) {
-      activarBotonReiniciarPartida();
-      mostrarMensaje("Felicidades encontraste todas las parejas");
-    }
-    resetearElEstadoPartida();
-  } else {
-    partida.intentos++;
-    setTimeout(() => {
-      parejaNoEncontrada(
-        tablero,
-        tablero.indiceCartaVolteadaA!,
-        tablero.indiceCartaVolteadaB!
-      );
-      mostrarIntentos();
-      ocultarImagenesDeParejaIncorrecta();
-      animacionesAlVoltearCarta(carta);
-      eliminarClasesDeAnimaciones();
-
-      resetearElEstadoPartida();
-    }, 1000);
+const verificacionDeParejas = (carta: Element): void => {
+  const indiceCartaA = tablero.indiceCartaVolteadaA;
+  const indiceCartaB = tablero.indiceCartaVolteadaB;
+  if (indiceCartaA !== undefined && indiceCartaB !== undefined) {
+    sonPareja(indiceCartaA, indiceCartaB, tablero)
+      ? laParejaEsEncontrada(tablero, indiceCartaA, indiceCartaB)
+      : laParejaNoEstaEncontrada(tablero, indiceCartaA, indiceCartaB, carta);
   }
+};
+
+const laParejaEsEncontrada = (
+  tablero: Tablero,
+  indiceCartaA: number,
+  indiceCartaB: number
+) => {
+  parejaEncontrada(tablero, indiceCartaA, indiceCartaB);
+  if (esPartidaCompleta(tablero)) {
+    activarBotonReiniciarPartida();
+    mostrarMensaje("Felicidades encontraste todas las parejas");
+  }
+  resetearElEstadoPartida();
+};
+
+const laParejaNoEstaEncontrada = (
+  tablero: Tablero,
+  indiceCartaA: number,
+  indiceCartaB: number,
+  carta: Element
+) => {
+  partida.intentos++;
+  setTimeout(() => {
+    parejaNoEncontrada(tablero, indiceCartaA, indiceCartaB);
+    mostrarIntentos();
+    ocultarImagenesDeParejaIncorrecta();
+    animacionesAlVoltearCarta(carta);
+    eliminarClasesDeAnimaciones();
+    resetearElEstadoPartida();
+  }, 1000);
 };
 
 export const manejarVolteoDeCarta = (
@@ -125,24 +129,11 @@ export const manejarVolteoDeCarta = (
   if (sePuedeVoltearLaCarta(tablero, indiceCarta)) {
     voltearLaCarta(tablero, indiceCarta);
     animacionesAlVoltearCarta(carta);
-    console.log(imagenCarta);
-    console.log(carta);
     imagenCarta.src = tablero.cartas[indiceImagen].imagen;
     mostrarMensaje("");
+    verificacionDeParejas(carta);
   } else {
     mostrarMensaje("Esta carta ya esta volteada.");
-    return;
-  }
-
-  if (tablero.estadoPartida === "CeroCartasLevantadas") {
-    tablero.indiceCartaVolteadaA = indiceCarta;
-    tablero.estadoPartida = "UnaCartaLevantada";
-    return;
-  }
-  if (tablero.estadoPartida === "UnaCartaLevantada") {
-    tablero.indiceCartaVolteadaB = indiceCarta;
-    tablero.estadoPartida = "DosCartasLevantadas";
-    verificacionDeParejas(carta);
   }
 };
 
@@ -167,33 +158,45 @@ const eliminarClasesAlCompletarPartida = () => {
     carta.classList.remove("animacion-voltear-carta");
   });
 };
-export const reiniciarPartida = () => {
+const ponerTodasLasCartasComoNoEncontradas = () => {
+  tablero.cartas.forEach((carta) => {
+    (carta.estaVuelta = false), (carta.encontrada = false);
+  });
+};
+const voltearTodasLasCartas = (cartas: NodeListOf<Element>) => {
+  cartas.forEach((carta) => {
+    if (carta instanceof HTMLImageElement) {
+      carta.src = "";
+    }
+  });
+};
+const gestionarSiSeVenLasCartasYIntentos = (estado: boolean) => {
   const contenedorCartas = document.getElementById("contenedor-cartas");
-  const cartas = document.querySelectorAll(".carta img");
   const divIntentos = document.getElementById("intentos");
-  eliminarClasesAlCompletarPartida();
-
-  if (
-    contenedorCartas instanceof HTMLDivElement &&
-    divIntentos instanceof HTMLDivElement &&
-    cartas
-  ) {
+  if (estado && contenedorCartas && divIntentos) {
+    contenedorCartas.style.display = "flex";
+    divIntentos.style.display = "block";
+  }
+  if (!estado && contenedorCartas && divIntentos) {
     contenedorCartas.style.display = "none";
     divIntentos.style.display = "none";
+  }
+};
+const reiniciarIntentos = () => {
+  partida.intentos = 0;
+  mostrarIntentos();
+}
+export const reiniciarPartida = () => {
+  const cartas = document.querySelectorAll(".carta img");
+  eliminarClasesAlCompletarPartida();
+  if (cartas) {
+    gestionarSiSeVenLasCartasYIntentos(false);
     iniciaPartida(tablero);
     resetearElEstadoPartida();
     mostrarMensaje("");
-    cartas.forEach((carta) => {
-      if (carta instanceof HTMLImageElement) {
-        carta.src = "";
-      }
-    });
-    tablero.cartas.forEach((carta) => {
-      (carta.estaVuelta = false), (carta.encontrada = false);
-    });
-    contenedorCartas.style.display = "flex";
-    divIntentos.style.display = "block";
-    partida.intentos = 0;
-    mostrarIntentos();
+    voltearTodasLasCartas(cartas);
+    ponerTodasLasCartasComoNoEncontradas();
+    gestionarSiSeVenLasCartasYIntentos(true);
+    reiniciarIntentos()
   }
 };
